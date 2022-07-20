a24a3f0c73607339fc60e1664e83c3b166d5d049
Rafael Soncine <rafinhaa@me.com>
2022-07-20 09:32:51 -0300

    Estrutura da Aplicação - Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
     </div>
   );
 };
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "success" | "danger";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  background-color: ${({ variant }) => buttonVariants[variant]};
+`;
