5708447cdf9673507840d0635ec425bd6d73bd1d
Rafael Soncine <rafinhaa@me.com>
2022-07-20 13:38:49 -0300

    Páginas e Rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
-import { Button } from './components/Button'
-
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
